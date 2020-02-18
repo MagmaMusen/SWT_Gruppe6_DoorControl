b6042ae335700f172ff2df37e325c69130a5e7ce
MagmaMusen <MagmaMusen@gmail.com>
2020-02-18 12:37:23 +0100

    Added test project and basic test

---
+++ DoorControl_Test_Unit/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoorControl_Test_Unit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test_OneEqualsOne_True()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
