988c8175f1accfa1fc219fef852ac749abee372e
GitDal <jeppedybdal@gmail.com>
2020-02-25 12:59:28 +0100

    Added empty functions to doorcontrol

--- DoorControl/DoorControl.cs
+++ DoorControl/DoorControl.cs
@@ -8,6 +8,49 @@ namespace DoorControl
 {
     public class DoorControl
     {
-        
+        public DoorControl()
+        {
+            Alarm = null;
+            Door = null;
+            EntryNotification = null;
+            UserValidation = null;
+        }
+
+        public void RequestEntry(string ID)
+        {
+
+        }
+
+        public void DoorOpen()
+        {
+
+        }
+
+        public void DoorClosed()
+        {
+
+        }
+
+        public void DoorOpened()
+        {
+
+        }
+
+
+
+        #region Properties
+        public IAlarm Alarm
+        { get; set; }
+
+        public IDoor Door
+        { get; set; }
+
+        public IEntryNotification EntryNotification
+        { get; set; }
+
+        public IUserValidation UserValidation
+        { get; set; }
+        #endregion
+
     }
 }
