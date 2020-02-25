2eefc63b8f1d14485f7c6bfbdc46cfffc342e6b6
GitDal <jeppedybdal@gmail.com>
2020-02-25 13:43:52 +0100

    Made corrections to SETUP in test project

--- DoorControl_Test_Unit/DoorControlUnitTest.cs
+++ DoorControl_Test_Unit/DoorControlUnitTest.cs
@@ -12,20 +12,42 @@ namespace DoorControl_Test_Unit
     [TestFixture]
     public class DoorControlUnitTest
     {
+
+        private DoorControl.DoorControl uut;
+        private IAlarm testAlarm;
+        private IDoor testDoor;
+        private IUserValidation testUserValidation;
+        private IEntryNotification testEntryNotification;
+
+
         [SetUp]
         public void Setup()
         {
-            DoorControl.DoorControl uut = new DoorControl.DoorControl();
-            uut.UserValidation = Substitute.For<IUserValidation>();
-            uut.Alarm = Substitute.For<IAlarm>();
-            uut.Door = Substitute.For<IDoor>();
-            uut.EntryNotification = Substitute.For<IEntryNotification>();
+            uut = new DoorControl.DoorControl();
+
+            //Test properties init
+            testAlarm = Substitute.For<IAlarm>();
+            testDoor = Substitute.For<IDoor>();
+            testUserValidation = Substitute.For<IUserValidation>();
+            testEntryNotification = Substitute.For<IEntryNotification>();
+
+            //Property injection
+            uut.Alarm = testAlarm;
+            uut.Door = testDoor;
+            uut.UserValidation = testUserValidation;
+            uut.EntryNotification = testEntryNotification;
         }
 
         [Test]
         public void Test_OneEqualsOne_True()
         {
             
+        }
+
+        [Test]
+        public void RequestEntry_InvalidID_NotifyEntryDeniedCalled()
+        {
+
         }
     }
 }
